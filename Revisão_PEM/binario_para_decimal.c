7593d867d274dea7d6830160d2c79479967a06a4
Mayara Dotta <dotta.may@gmail.com>
2021-02-20 08:05:32 -0300

    update_revisao

--- Revisão_PEM/binario_para_decimal.c
+++ Revisão_PEM/binario_para_decimal.c
@@ -36,7 +36,43 @@ int main(int argc, char *argv[]){
 		printf("%d\n", mult);
 		n= n/10;
 		mult = mult * 2;
-	}*/
+	}
+	printf("\nConversao em decimal = %d\n", soma);*/
 	
+    /*OU 
+    int n, //número a converter
+    copia, //cópia do valor original
+    resto, //resto da divisão por 10
+    quociente, // resultado da divisão por 10
+    potenciaDe2 = 1, //primeira potência de 2 .... 2^0 = 1
+    soma = 0; //soma acumulada das parcelas da conversão
+    
+    printf("Entre com um número em binário:");
+    scanf("%d", &n);
+    copia = n;
+    while(n>0){
+        quociente = n/10;
+        resto = n%10;
+        soma = soma + resto * potenciaDe2;
+        n = quociente;
+        potenciaDe2 = potenciaDe2 * 2;
+    }
+    printf("%d corresponde a %d", copia, soma);*/
+
+    /*OU usando strings:
+    #include <stdio.h>
+    #include <string.h>
+    int main(){   
+    char entrada[50];
+    printf("Entre com o número em binário:");
+    gets(entrada);
+    int tamanho = strlen(entrada);
+    int i, soma = 0, potenciaDe2 = 1;
+    for(i=tamanho-1; i>=0; i--)
+    {   soma = soma + (entrada[i] - '0')*potenciaDe2;
+        potenciaDe2 = potenciaDe2 * 2;
+    }
+    printf("Resultado = %d\n", soma);*/
+
     return 0;
 }
